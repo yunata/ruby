9ace65e804d21531cea26919d24c665137c9b5ce
yuuichirou <yuuichirou.nata@gmail.com>
2024-05-22 18:28:00 +0900

    Add layout links

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  require 'debug'
   # before_action :set_article, only: %i[ show edit update destroy ]
 
   # GET /articles or /articles.json
@@ -9,6 +10,38 @@ class ArticlesController < ApplicationController
   # GET /articles/1 or /articles/1.json
   def show
     @article = Article.find(params[:id])
+  end
+
+  # GET /articles/new
+  def new
+    @article = Article.new
+  end
+
+  # GET /articles/create
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article.save
+    redirect_to @article
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article was updated successfully."
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
   end
 
 end
